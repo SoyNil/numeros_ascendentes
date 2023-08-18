0b081d99e2b7e3efafac57dc428c6985f39f3893
SoyNil <1422624@senati.pe>
2023-08-17 19:37:44 -0500

    Orden cambiado

--- Numeros_Ascendentes/src/numeros_ascendentes/Numeros_Ascendentes.java
+++ Numeros_Ascendentes/src/numeros_ascendentes/Numeros_Ascendentes.java
@@ -4,16 +4,15 @@ import javax.swing.JOptionPane;
 public class Numeros_Ascendentes {
     public static void main(String[] args) {
         // TODO code application logic here
-        
         int numeros[]  = new int[5];
         for(int i=0; i<numeros.length; i++)
         {   
-            numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer número"));
+            numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero "));
         }
         Arrays.sort(numeros);
         for(int i = 0; i < numeros.length; i++)
         {
-            System.out.println(numeros[i]);
+            System.out.print(numeros[i]+" ");
         }
 }}
 
